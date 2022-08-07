37514aea23890fabf477673ba740f6b1c75f6bcf
jess-figueiredo <jhessikafeitosa@hotmail.com.br>
2022-08-07 19:20:40 -0300

    first commit

--- src/loops/Ex5_Tabuada.java
+++ src/loops/Ex5_Tabuada.java
@@ -9,7 +9,7 @@ public class Ex5_Tabuada {
         System.out.println("Tabuada: ");
         int tabuada = scan.nextInt();
 
-        System.out.println("Tabuada do nº: "+ tabuada);
+        System.out.println("Tabuada do número: "+ tabuada);
 
         for(int i=1 ; i <= 10 ; i++){
             System.out.println(tabuada + " x "+ i + " = " +(tabuada*i));
